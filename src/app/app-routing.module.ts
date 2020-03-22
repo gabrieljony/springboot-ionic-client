3474c3c4f4b7b6ab22139bfe247e5581b5b7e9a7
Gabriel Jony <gabjony@gmail.com>
2020-03-22 16:24:54 -0300

    3 ionic generate page categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },  {
+    path: 'categorias',
+    loadChildren: () => import('./categorias/categorias.module').then( m => m.CategoriasPageModule)
   }
+
 ];
 
 @NgModule({
