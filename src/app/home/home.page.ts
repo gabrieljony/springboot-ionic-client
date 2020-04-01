22d56688bd817fe3fbc03b17993cf629af4e0d0b
Gabriel Jony <gabjony@gmail.com>
2020-04-01 09:30:40 -0300

    9 Obtendo os dados do formulário de login [(ngModel)]

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, MenuController } from '@ionic/angular';
+import { CredenciaisDTO } from 'src/models/credenciais.dto';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,11 @@ import { NavController, MenuController } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
 
   constructor(public navCtrl: NavController
     , public menu: MenuController) { }
@@ -23,6 +29,7 @@ export class HomePage implements OnInit {
   }
 
   login() {
+    console.log(this.creds)
     this.navCtrl.navigateRoot('/categorias');
   }
 
