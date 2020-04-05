59b20cf93f6dd02fa8047e65490d86674896093c
Gabriel Jony <gabjony@gmail.com>
2020-04-05 11:06:24 -0300

    19 Botão de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,6 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
       title: 'Categorias',
       url: '/categorias',
       icon: 'home'
@@ -26,13 +22,20 @@ export class AppComponent implements OnInit {
       title: 'Profile',
       url: '/profile',
       icon: 'home'
+    },
+    {
+      title: 'Logout',
+      url: '/',
+      icon: 'home'
     }
   ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private auth: AuthService,
+    public navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -45,6 +48,20 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+
+  }
+
+  openPage(page: { title: string, url: string }) {
+    console.log(page)
+    switch (page.title) {
+      case 'Logout':
+        this.auth.logout();
+        this.navCtrl.navigateRoot(page.url);
+        break;
+
+      default:
+        this.navCtrl.navigateRoot(page.url);
+    }
 
   }
 }
