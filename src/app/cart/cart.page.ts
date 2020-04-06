80ee127ac527d8ffa986fa212fe1d25c30bb5423
Gabriel Jony <gabjony@gmail.com>
2020-04-06 18:52:12 -0300

    31 Terminando as funcionalidades do carrinho

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { CartItem } from 'src/models/cart-item';
 import { ProdutoService } from 'src/services/domain/produto.service';
 import { environment } from 'src/environments/environment';
 import { CartService } from 'src/services/domain/cart.service';
+import { ProdutoDTO } from 'src/models/produto.dto';
 
 @Component({
   selector: 'app-cart',
@@ -14,7 +17,8 @@ export class CartPage implements OnInit {
   items: CartItem[];
 
   constructor(public cartService: CartService,
-    public produtoService: ProdutoService) { }
+    public produtoService: ProdutoService,
+    public router: Router) { }
 
   ngOnInit() {
     let cart = this.cartService.getCart();
@@ -26,10 +30,33 @@ export class CartPage implements OnInit {
     for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.produto.id)
-      .subscribe(response => {
-        item.produto.imageUrl = `${environment.bucketAmazonS3}/prod${item.produto.id}-small.jpg`;
-      }, error => { });
+        .subscribe(response => {
+          item.produto.imageUrl = `${environment.bucketAmazonS3}/prod${item.produto.id}-small.jpg`;
+        }, error => { });
     }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  descreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total(): number {
+    return this.cartService.total();
+  }
+
+  /**
+   * Método de continuar comprando
+   */
+  goOn() {
+    this.router.navigate(['/categorias']);
   }
 
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment';
 import { StorageService } from './storage.service';
 import { LocalUser } from 'src/models/local_user';
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { CartService } from './domain/cart.service';
 
 @Injectable()
 export class AuthService {
@@ -12,38 +13,40 @@ export class AuthService {
   jwtHelperService: JwtHelperService = new JwtHelperService();
 
   constructor(public http: HttpClient
-    , public storage: StorageService){
+    , public storage: StorageService,
+    public cartService: CartService) {
 
   }
 
-  authenticate(creds: CredenciaisDTO){
-    return this.http.post(`${environment.API_CONFIG}/login`, 
-    creds,
-    {
-      observe: 'response',
-      responseType: 'text'
-    })
+  authenticate(creds: CredenciaisDTO) {
+    return this.http.post(`${environment.API_CONFIG}/login`,
+      creds,
+      {
+        observe: 'response',
+        responseType: 'text'
+      })
   }
 
-  refreshToken(){
-    return this.http.post(`${environment.API_CONFIG}/auth/refresh_token`, 
-    {},
-    {
-      observe: 'response',
-      responseType: 'text'
-    })
+  refreshToken() {
+    return this.http.post(`${environment.API_CONFIG}/auth/refresh_token`,
+      {},
+      {
+        observe: 'response',
+        responseType: 'text'
+      })
   }
 
-  successFullLogin(authorizationValue: string){
+  successFullLogin(authorizationValue: string) {
     let token = authorizationValue.substring(7);
     let user: LocalUser = {
       token: token,
       email: this.jwtHelperService.decodeToken(token).sub
     };
     this.storage.setLocalUser(user);
+    this.cartService.createOrClearCart();
   }
 
-  logout(){
+  logout() {
     this.storage.setLocalUser(null);
   }
 }
