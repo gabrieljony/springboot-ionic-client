8076e2eaa41d309040a8b672cff94f95952e8996
Gabriel Jony <gabjony@gmail.com>
2020-04-14 10:55:17 -0300

    40 Componente loading ao carregar os produtos

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProdutoDTO } from 'src/models/produto.dto';
 import { ProdutoService } from 'src/services/domain/produto.service';
 import { environment } from 'src/environments/environment';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-produtos',
@@ -17,18 +18,15 @@ export class ProdutosPage implements OnInit {
 
   constructor(public produtoService: ProdutoService,
     private route: ActivatedRoute,
-    public router: Router) { }
+    public router: Router,
+    public loadingController: LoadingController) { }
 
   ngOnInit() {
     //Pegar o paramentro que passa pela navegação, extraindo parâmetros de url
     this.route.queryParams.subscribe((queryParams: any) => {
       this.categoriaId = queryParams['categoria_id'];
     });
-    this.produtoService.findByCategoria(this.categoriaId)
-      .subscribe(response => {
-        this.items = response['content'];
-        this.loadImageurls();
-      }, error => { })
+    this.presentLoading()
   }
 
   loadImageurls() {
@@ -41,8 +39,37 @@ export class ProdutosPage implements OnInit {
   }
 
   showDetail(produto_id: string) {
-    this.router.navigate(['/produto-detail'], { queryParams: {'produto_id': produto_id }});
-  
+    this.router.navigate(['/produto-detail'], { queryParams: { 'produto_id': produto_id } });
+
+  }
+
+
+// https://ionicframework.com/docs/api/loading
+  async presentLoading() {
+    let loading: any;
+    try {
+      loading = await this.loadingController.create({ message: 'Aguarde...' });
+      await loading.present();
+
+      this.produtoService.findByCategoria(this.categoriaId)
+        .subscribe(response => {
+          this.items = response['content'];
+          if (loading) {
+            loading.dismiss();
+          }
+          this.loadImageurls();
+        }, error => {
+          if (loading) {
+            loading.dismiss();
+          }
+          console.log('Failed FirebasePage: ' + JSON.stringify(error, ['message', 'arguments', 'type', 'name']));
+        })
+    } catch (err) {
+      if (loading) {
+        loading.dismiss();
+      }
+      console.log('Failed FirebasePage: ' + JSON.stringify(err, ['message', 'arguments', 'type', 'name']));
+    }
   }
 
 }
